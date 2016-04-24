f5254edca2add7f4968516cc3905cad7e014263b
amaurs <amaury.gtz@gmail.com>
2016-04-23 21:29:11 -0500

    Fixed wrong address in path.

--- scripts/download_by_year.sh
+++ scripts/download_by_year.sh
@@ -10,7 +10,7 @@ do
 	for table in 501 502 503 504 505 507 509 510 512 520 551 552 553 554 556 557
 	do
 		echo curl ftp://ftp2.sat.gob.mx/aduanas/$year/Balanza_publica/$month/t$table.zip -O >> $base/download_$year.sh
-		echo aws s3 cp t$table.zip s3://aduanas/aduanas/$year/Balanza_publica/$month/ >> $base/download_$year.sh
+		echo aws s3 cp t$table.zip s3://aduanas/$year/Balanza_publica/$month/ >> $base/download_$year.sh
 		echo rm cp t$table.zip >> $base/download_$year.sh
 	done
 done
